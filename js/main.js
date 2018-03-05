3cc3d7c39252aeee7762727660063e1b5561015e
Zuzana K <zk0433@gmail.com>
2018-03-05 12:05:07 +0000

    refactor flex code

--- js/main.js
+++ js/main.js
@@ -138,30 +138,35 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
 
+  const wrapper = document.createElement('div');
+  wrapper.className = 'restaurant-list__wrapper';
+  li.append(wrapper);
+
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
+  image.alt = `Image of the ${restaurant.name} restaurant`;
+  wrapper.append(image);
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  wrapper.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  wrapper.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  wrapper.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  wrapper.append(more);
 
   return li
-}
+} 
 
 /**
  * Add markers for current restaurants to the map.
